444b361caaf004558d14b067911fea4007c3250f
Parasite <ansuman0026@gmail.com>
2024-12-09 15:13:23 +0530

    added tsl

--- database/database.py
+++ database/database.py
@@ -11,7 +11,7 @@ uri = os.getenv("uri")
 
 
 # Create a new client and connect to the server
-client = MongoClient(uri, server_api=ServerApi('1'), tlsCAFile=certifi.where())
+client = MongoClient(uri, server_api=ServerApi('1'),tls=True, tlsCAFile=certifi.where())
 
 
 
