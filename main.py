ffe823d4161caa7209b1f34f1c34afcd45df9a30
Parasite <ansuman0026@gmail.com>
2024-12-11 13:37:07 +0530

    CORS fixxed

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from api.mcqQuestionRouter import router as mcq_router
 from api.tfQuestionRouter import router as tf_router
 from api.aiRouter import router as ai_router
 from api.fillQUestionRouter import router as fill_router
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:5173",
+    "https://leanlearn01.vercel.app",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],     # Allow all HTTP methods
+    allow_headers=["*"],     # Allow all headers
+)
 app.include_router(tf_router)
 app.include_router(mcq_router)
 app.include_router(fill_router)
