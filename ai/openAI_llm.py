79c40a9021e5be201c3aeb4bd07a40e273dac824
Bindra01 <162609724+Bindra01@users.noreply.github.com>
2025-01-27 10:30:44 +0530

    Update openAI_llm.py

--- ai/openAI_llm.py
+++ ai/openAI_llm.py
@@ -1,7 +1,7 @@
-from langchain.chat_models import ChatOpenAI
-from langchain.schema import SystemMessage, AIMessage, HumanMessage
+rom langchain_openai import ChatOpenAI
+from langchain_core.messages import SystemMessage, AIMessage, HumanMessage
 import os
-from dotenv import load_dotenv
+from dotenv import load_dotenv 
 
 load_dotenv()
 
